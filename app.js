58acd558679ff44237c1bfe259d1926449000e82
Vladislav-07 <143828866+Vladislav-07@users.noreply.github.com>
2024-01-27 14:44:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let bascketItem = []
 buttonPhoneOne.addEventListener('click', function() {
   swal("Успешно!", "Товар 'Samsung Galaxy S22' успешно добавлен в корзину! Для удаления, нажмите еще раз", "success");
   bascketItem.push('Samsung Galaxy S22')
-  let itemPhoneOne = console.log(bascketItem.includes('Samsung Galaxy S22')); 
+  let itemPhoneOne = bascketItem.includes('Samsung Galaxy S22'); 
   if (itemPhoneOne !== true) {
     let cardBascket = document.querySelector('.card-bascket1')
     cardBascket.classList.toggle('active')
@@ -20,7 +20,7 @@ buttonPhoneOne.addEventListener('click', function() {
 buttonTvOne.addEventListener('click', function() {
   swal("Успешно!", "Товар 'Samsung 32 inches QLED 4K Smart TV' успешно добавлен в корзину! Для удаления, нажмите еще раз", "success");
   bascketItem.push('Samsung 32 inches QLED 4K Smart TV')
-  let itemTvOne = console.log(bascketItem.includes('Samsung 32 inches QLED 4K Smart TV')); 
+  let itemTvOne = bascketItem.includes('Samsung 32 inches QLED 4K Smart TV'); 
   if (itemTvOne !== true) {
     let cardBascket = document.querySelector('.card-bascket2')
     cardBascket.classList.toggle('active')
@@ -30,7 +30,7 @@ buttonTvOne.addEventListener('click', function() {
 buttonPcOne.addEventListener('click', function() {
   swal("Успешно!", "Товар '2021 Apple MacBook Pro' успешно добавлен в корзину! Для удаления, нажмите еще раз", "success");
   bascketItem.push('2021 Apple MacBook Pro')
-  let itemPPcOne = console.log(bascketItem.includes('2021 Apple MacBook Pro')); 
+  let itemPPcOne = bascketItem.includes('2021 Apple MacBook Pro'); 
   if (itemPPcOne !== true) {
     let cardBascket = document.querySelector('.card-bascket3')
     cardBascket.classList.toggle('active')
@@ -40,7 +40,7 @@ buttonPcOne.addEventListener('click', function() {
 buttonPhoneTwo.addEventListener('click', function() {
   swal("Успешно!", "Товар 'iPhone 12' успешно добавлен в корзину! Для удаления, нажмите еще раз", "success");
   bascketItem.push('iPhone 12')
-  let itemPhoneTwo = console.log(bascketItem.includes('iPhone 12')); 
+  let itemPhoneTwo = bascketItem.includes('iPhone 12'); 
   if (itemPhoneTwo !== true) {
     let cardBascket = document.querySelector('.card-bascket4')
     cardBascket.classList.toggle('active')
@@ -50,7 +50,7 @@ buttonPhoneTwo.addEventListener('click', function() {
 buttonTvTwo.addEventListener('click', function() {
   swal("Успешно!", "Товар 'Sony X85J 55 Inch smart TV' успешно добавлен в корзину! Для удаления, нажмите еще раз", "success");
   bascketItem.push('Sony X85J 55 Inch smart TV')
-  let itemTvTwo = console.log(bascketItem.includes('Sony X85J 55 Inch smart TV')); 
+  let itemTvTwo = bascketItem.includes('Sony X85J 55 Inch smart TV'); 
   if (itemTvTwo !== true) {
     let cardBascket = document.querySelector('.card-bascket5')
     cardBascket.classList.toggle('active')
@@ -60,7 +60,7 @@ buttonTvTwo.addEventListener('click', function() {
 buttonPcTwo.addEventListener('click', function() {
   swal("Успешно!", "Товар 'SAMSUNG Galaxy Book Pro' успешно добавлен в корзину! Для удаления, нажмите еще раз", "success");
   bascketItem.push('SAMSUNG Galaxy Book Pro')
-  let itemPcTwo = console.log(bascketItem.includes('SAMSUNG Galaxy Book Pro')); 
+  let itemPcTwo = bascketItem.includes('SAMSUNG Galaxy Book Pro'); 
   if (itemPcTwo !== true) {
     let cardBascket = document.querySelector('.card-bascket6')
     cardBascket.classList.toggle('active')
